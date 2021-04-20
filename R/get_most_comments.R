8acb717d39b39cbb7fd09482cc4e27f34a3b50f2
Balthasar <balthasar.sager@gmail.com>
2021-04-20 15:11:49 +0200

    divide up into helpers and main functions, documentation, etc.

--- R/get_most_comments.R
+++ R/get_most_comments.R
@@ -38,59 +38,8 @@ parse_replies <- function(comment_thread) {
   }
 }
 
-#' Get Most Comments
-#'
-#' @param filter string; Required.
-#' named vector of length 1
-#' potential names of the entry in the vector:
-#' \code{video_id}: video ID.
-#' \code{channel_id}: channel ID.
-#' \code{thread_id}: comma-separated list of comment thread IDs
-#' \code{threads_related_to_channel}: channel ID.
-#'
-#' @param part  Comment resource requested. Required. Comma separated list
-#' of one or more of the
-#' following: \code{id, snippet}. e.g., \code{"id, snippet"},
-#' \code{"id"}, etc. Default: \code{snippet}.
-#' @param max_results  Maximum number of items that should be returned.
-#'  Integer. Optional. Default is 100.
-#' If the value is greater than 100 then the function fetches all the
-#' results. The outcome is a simplified \code{data.frame}.
-#' @param page_token  Specific page in the result set that should be
-#' returned. Optional.
-#' @param text_format Data Type: Character. Default is \code{"html"}.
-#' Only takes \code{"html"} or \code{"plainText"}. Optional.
-#' @param \dots Additional arguments passed to \code{\link{tuber_GET}}.
-#'
-#' @return
-#' Nested named list. The entry \code{items} is a list of comments
-#' along with meta information.
-#' Within each of the \code{items} is an item \code{snippet} which
-#' has an item \code{topLevelComment$snippet$textDisplay}
-#' that contains the actual comment.
-#'
-#' If simplify is \code{TRUE}, a \code{data.frame} with the following columns:
-#' \code{authorDisplayName, authorProfileImageUrl, authorChannelUrl,
-#' authorChannelId.value, videoId, textDisplay,
-#' canRate, viewerRating, likeCount, publishedAt, updatedAt}
-#'
-#' @export get_most_comments
-#'
-#' @references \url{https://developers.google.com/youtube/v3/docs/commentThreads/list}
-#'
-#' @examples
-#' \dontrun{
-#'
-#' # Set API token via yt_oauth() first
-#'
-#' get_most_comments(filter = c(video_id = "N708P-A45D0"))
-#' get_most_comments(filter = c(video_id = "N708P-A45D0"), max_results = 101)
-#' }
-#'
-#'
-
-get_most_comments <- function(filter = NULL, part = "snippet,replies,id",
-                              text_format = "html", max_results = 100, page_token = NULL, ...) {
+get_parse_bind_comments <- function(filter = NULL, page_token = NULL,
+                                    part = part, text_format = text_format, max_results = 100) {
   if (max_results < 20) {
     stop("max_results only takes a value over 20.
             Above 100, it outputs all the results.")
@@ -101,7 +50,7 @@ get_most_comments <- function(filter = NULL, part = "snippet,replies,id",
   }
 
   if (!(names(filter) %in%
-    c("video_id", "channel_id", "thread_id", "threads_related_to_channel"))) {
+        c("video_id", "channel_id", "thread_id", "threads_related_to_channel"))) {
     stop("filter can only take one of values: channel_id, video_id, parent_id,
         threads_related_to_channel.")
   }
@@ -129,12 +78,12 @@ get_most_comments <- function(filter = NULL, part = "snippet,replies,id",
   )
 
   querylist <- c(querylist, filter)
-  print(querylist)
+  # print(querylist)
 
   ## get first page of results of a comment thread and
   ## initialize objects with content of first page before
   ## proceeding to next pages of API response
-  res <- tuber:::tuber_GET("commentThreads", querylist, ...)
+  res <- tuber_GET("commentThreads", querylist)
   # parse results
   snippet <- parse_snippet(res)
   comment_thread <- parse_comment_thread(snippet)
@@ -162,57 +111,67 @@ get_most_comments <- function(filter = NULL, part = "snippet,replies,id",
 
   #   # shouldn't this be `unique()`?
   # next_page_token <- unique(res$nextPageToken)
-  print("erstes Mal")
-  print(next_page_token)
+  # print("erstes Mal")
+  # print(next_page_token)
 
-  print("finished")
+  # print("finished")
 
   comments
 }
 
-library(magrittr)
-tuber::yt_oauth(
-  app_id = Sys.getenv("YOUTUBE_API_APP_ID"),
-  app_secret = Sys.getenv("YOUTUBE_API_CLIENT_SECRET")
-)
 
-all_data <- get_most_comments(filter = c(video_id = "Hop_MfkXl7c"), page_token = NULL)
-counter_while <- 0
-# next_page_token <- NULL
-next_page_token <- unique(all_data$nextPageToken)
-
-while (counter_while == 0 | !is.null(next_page_token)) {
-  next_data <- get_most_comments(
-    filter = c(video_id = "Hop_MfkXl7c"),
-    page_token = next_page_token
-  )
-  next_page_token <- unique(next_data$nextPageToken)
-  print(next_data)
-  counter_while <- counter_while + 1
-  message(paste(counter_while, ":", "counter_while"))
-  message(paste(next_page_token, ":", "next_page_token"))
-  all_data <- dplyr::bind_rows(next_data, all_data)
-}
-return(all_data)
-
-retrieve_data_from_paginated_api <- function(video_id_input) {
+#' Get Most Comments
+#'
+#' Retrieves all top level comments and replies to them.
+#' Replies to replies are not included.
+#' @param video_id ID of video, required.
+#' @return
+#' Data frame with all comments and replies.
+#'
+#' @export get_most_comments
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # Set API token via yt_oauth() first
+#'
+#' get_most_comments(video_id = "Hop_MfkXl7c")
+#' }
+#'
+#'
+#'
+get_most_comments <- function(video_id) {
+  video_id_arg <- video_id
+  part_arg <- "snippet,replies,id"
+  filter_arg <- c(video_id = video_id_arg)
+  text_format_arg <- "html"
 
   # initialize objects for loop
-  all_data <- get_most_comments(filter = c(video_id = video_id_input), page_token = NULL)
+  all_data <- get_parse_bind_comments(
+    filter = filter_arg,
+    page_token = NULL,
+    part = part_arg,
+    text_format = text_format_arg,
+    max_results = 100
+    )
   counter_while <- 0
-  next_page_token <- unique(all_data$nextPageToken)
+  suppressWarnings(next_page_token <- unique(all_data$nextPageToken))
 
   # loop over results until last nextPageToken
   while (counter_while == 0 | !is.null(next_page_token)) {
-    next_data <- get_most_comments(
-      filter = c(video_id = video_id_input),
-      page_token = next_page_token
+    next_data <- get_parse_bind_comments(
+      filter = filter_arg,
+      page_token = next_page_token,
+      part = part_arg,
+      text_format = text_format_arg,
+      max_results = 100
     )
+    counter_while <- counter_while + 1
+    # cli::cli_alert_success("Page {counter_while} packages.")
 
     # overwrite `next_page_token` that was initialized outside loop
     # with new content that was just retrieved in the data
-    next_page_token <- unique(next_data$nextPageToken)
-    counter_while <- counter_while + 1
+    suppressWarnings(next_page_token <- unique(all_data$nextPageToken))
 
     # overwrite `all_data` that was initialized outside loop
     # using `all_data` from outside of loop in first iteration
@@ -223,28 +182,8 @@ retrieve_data_from_paginated_api <- function(video_id_input) {
   return(all_data)
 }
 
-# new_example_with_function <- retrieve_data_from_paginated_api(video_id_input = "Hop_MfkXl7c")
-# new_example_with_function %>%
-#   dplyr::filter(totalReplyCount > 1 & is_reply)
-# length(unique(new_example_with_function$snippet_textOriginal))
-
-# example <- get_most_comments(filter = c(video_id = "Hop_MfkXl7c"), page_token = NULL)
-# example %>%
-#   tidyr::unnest(nextPageToken)
-#
-# get_most_comments(filter = c(video_id = "Hop_MfkXl7c"), page_token = "QURTSl9pMjVDT2V3WGF5Nm5ha3ZYam1HbWVPMFVybTJuWk96R2UyOTZwNnVwSXFJdTJfUFVpSVI3VUxqbU1TSGpKWVpCcFpITEl4cm83dw==")
-# tuber::get_comment_threads(filter = c(video_id = "Hop_MfkXl7c")) %>%
-#   tibble::as_tibble()
-#
-# res_2 <- get_most_comments(filter = c(video_id = "Hop_MfkXl7c"))
-#
-# res_2$nextPageToken
-#
-# t1 <- "QURTSl9pMnM3TFpxY2FQUTVDNGVfTTBDRUF0Nm52R0RXNGRuM1R3a21fMDZhemR0aUtDeHRTWnV3UXpmREs0cnI0TmYzOXh2VTlzRXdOYw=="
-# t2 <- "QURTSl9pMHdOWXBXWlRBakdlQnk1VXBUNEljSXM0QTU0WkNuOEp2VFBiX0RMQlhMeDVGdEo1UTlpWm5BaEFTRGZZZWU4UmV0LVFMTmFydw=="
-# t3 <- "QURTSl9pMlY5M1Q1VkxsZW9RbXR0VG1acXkzRjRMaDYxcEc4c05UenVUc0VIR0tXbmZmVHU2V3RVdC1WUmlHaW5wa19WUlJfS19vSGEzNA=="
-#
-# get_most_comments(filter = c(video_id = "Hop_MfkXl7c"), page_token = t1)
-# get_most_comments(filter = c(video_id = "Hop_MfkXl7c"), page_token = t2)
-# get_most_comments(filter = c(video_id = "Hop_MfkXl7c"), page_token = t3)
-#
+# tuber::yt_oauth(
+#   app_id = Sys.getenv("YOUTUBE_API_APP_ID"),
+#   app_secret = Sys.getenv("YOUTUBE_API_CLIENT_SECRET")
+# )
+# get_most_comments(video_id = "Hop_MfkXl7c")
--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,3 @@
 # fix warning about no visible bindings due to tidyverse functions
 # https://community.rstudio.com/t/how-to-solve-no-visible-binding-for-global-variable-note/28887
-utils::globalVariables(c("totalReplyCount", "id", "comments", "kind", "etag", "items", "snippet"))
+utils::globalVariables(c("totalReplyCount", "id", "comments", "kind", "etag", "items", "snippet", "video_id_arg"))
